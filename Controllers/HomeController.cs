97faae8bd5610f6fc23e88713eec8f44a780ea4a
Chengkai Yang <yangchengkai@gmail.com>
2021-02-04 21:25:09 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using AzureAppServiceTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+
+namespace AzureAppServiceTest.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      //MVC default view template is the same as action method, Index
+      public IActionResult Index()
+      {
+         //Use ViewData to pass to View
+         var sb = new StringBuilder();
+         sb.AppendLine("CHYA Azure App Service Test");
+         sb.AppendLine("Server: " + Environment.MachineName);
+         sb.AppendLine("Server OS: " + Environment.OSVersion);
+         sb.AppendLine(".Net version: " + RuntimeInformation.FrameworkDescription);
+
+         ViewData["Message"] = sb.ToString();
+         ViewData["Time"] = DateTime.Now.ToLongTimeString();
+
+         return View();
+      }
+
+      public IActionResult Privacy()
+      {
+         ViewData["PrivacyMessage"] = "This is a test privacy message of CHYA";
+         return View();
+      }
+
+      public IEnumerable<string> Test(string msg)
+      {
+         var res = new List<string>()
+                   {
+                      msg,
+                      "AAA",
+                      "BBB"
+                   };
+         return res;
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAppServiceTest.Models
+{
+   public class ErrorViewModel
+   {
+      public string RequestId { get; set; }
+
+      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+   }
+}
